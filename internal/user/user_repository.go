6c5fe362e94852fba7d8e9fb3de2b7b912dc5bcf
Adnan Hashmi <hashmi.adnan791@gmail.com>
2023-05-15 12:06:18 +0530

    cleaned the queries in repository

--- internal/user/user_repository.go
+++ internal/user/user_repository.go
@@ -25,7 +25,12 @@ func (r *Repository) CreateUser(ctx context.Context, user *User) (*User, error) 
 	// Check if username already exists
 	var uid gocql.UUID
 	duplicate := true
-	if err := r.db.Query(`SELECT id FROM users WHERE username=? ALLOW FILTERING`, user.Username).WithContext(ctx).Scan(&uid); err != nil {
+	if err := r.db.Query(`
+            SELECT id 
+            FROM users 
+            WHERE username=? 
+            ALLOW FILTERING`,
+		user.Username).WithContext(ctx).Scan(&uid); err != nil {
 
 		if err != gocql.ErrNotFound {
 			log.Println("Error occured while executing query to get if username exists or not in create user repository function:", err)
@@ -38,10 +43,16 @@ func (r *Repository) CreateUser(ctx context.Context, user *User) (*User, error) 
 	if duplicate == true {
 		return nil, errors.New("username already exists.")
 	}
+
 	// Check if email already exists
 	duplicate = true
 	var email_check string
-	if err := r.db.Query(`SELECT ID FROM users WHERE email=? ALLOW FILTERING`, user.Email).WithContext(ctx).Scan(&email_check); err != nil {
+	if err := r.db.Query(`
+            SELECT ID 
+            FROM users 
+            WHERE email=? 
+            ALLOW FILTERING`,
+		user.Email).WithContext(ctx).Scan(&email_check); err != nil {
 
 		if err != gocql.ErrNotFound {
 			log.Println("Error occured while executing query to get if email exists or not in create user repository function:", err)
@@ -57,8 +68,10 @@ func (r *Repository) CreateUser(ctx context.Context, user *User) (*User, error) 
 
 	newId, _ := gocql.RandomUUID()
 
-	if err := r.db.Query(`INSERT INTO users (id, username, email, name, password) 
-                       VALUES (?, ?, ?, ?, ?)`,
+	if err := r.db.Query(`
+            INSERT INTO 
+            users (id, username, email, name, password) 
+            VALUES (?, ?, ?, ?, ?)`,
 		newId, user.Username, user.Email, user.Name, user.Password).WithContext(ctx).Exec(); err != nil {
 		log.Fatal(err)
 	}
@@ -69,7 +82,13 @@ func (r *Repository) CreateUser(ctx context.Context, user *User) (*User, error) 
 
 func (r *Repository) GetUserByEmail(ctx context.Context, email string) (*User, error) {
 	u := User{}
-	scanner := r.db.Query(`SELECT id, username, email, name, password FROM users WHERE email=? ALLOW FILTERING`, email).WithContext(ctx).Iter().Scanner()
+	scanner := r.db.Query(`
+             SELECT 
+             id, username, email, name, password 
+             FROM users 
+             WHERE email=? 
+             ALLOW FILTERING`,
+		email).WithContext(ctx).Iter().Scanner()
 	scanner.Next()
 
 	err := scanner.Scan(&u.ID, &u.Username, &u.Email, &u.Name, &u.Password)
--- internal/ws/ws_repository.go
+++ internal/ws/ws_repository.go
@@ -26,7 +26,12 @@ func (r *Repository) CreateRoom(ctx context.Context, room *Room, default_channel
 		admin_email    string
 	)
 
-	if err := r.db.Query(`SELECT id, username, email FROM users WHERE id=? ALLOW FILTERING`,
+	if err := r.db.Query(`
+            SELECT 
+            id, username, email 
+            FROM users 
+            WHERE id=? 
+            ALLOW FILTERING`,
 		room.Admin).WithContext(ctx).Scan(&uid, &admin_username, &admin_email); err != nil {
 		if err != gocql.ErrNotFound {
 			log.Println("Error occured while executing query to get if admin exists or not in create room repository function:", err)
@@ -39,7 +44,12 @@ func (r *Repository) CreateRoom(ctx context.Context, room *Room, default_channel
 	// Check if room name already exists for that admin
 	var room_id string
 
-	if err := r.db.Query(`SELECT id FROM rooms WHERE admin=? AND room_name=? ALLOW FILTERING`,
+	if err := r.db.Query(`
+            SELECT 
+            id 
+            FROM rooms 
+            WHERE admin=? AND room_name=? 
+            ALLOW FILTERING`,
 		room.Admin, room.RoomName).WithContext(ctx).Scan(&room_id); err != nil {
 		if err != gocql.ErrNotFound {
 			log.Println("Error occured while executing query to get if admin exists or not in create room repository function:", err)
@@ -54,20 +64,28 @@ func (r *Repository) CreateRoom(ctx context.Context, room *Room, default_channel
 
 	batch := r.db.NewBatch(gocql.LoggedBatch).WithContext(ctx)
 	batch.Entries = append(batch.Entries, gocql.BatchEntry{
-		Stmt:       "INSERT INTO rooms (id, room_name, channels, members, admin, created_at) VALUES (?, ?, ?, ?, ?, ?)",
+		Stmt: `INSERT 
+           INTO rooms 
+           (id, room_name, channels, members, admin, created_at) 
+           VALUES (?, ?, ?, ?, ?, ?)`,
 		Args:       []interface{}{room.ID, room.RoomName, room.Channels, room.Members, room.Admin, room.CreatedAt},
 		Idempotent: true,
 	})
 
 	batch.Entries = append(batch.Entries, gocql.BatchEntry{
-		Stmt: "INSERT INTO channels (id, channel_name, is_direct_channel, members, created_at, messages) VALUES (?, ?, ?, ?, ?, ?)",
+		Stmt: `INSERT 
+           INTO channels 
+           (id, channel_name, is_direct_channel, members, created_at, messages) 
+           VALUES (?, ?, ?, ?, ?, ?)`,
 		Args: []interface{}{default_channel.ID, default_channel.ChannelName, default_channel.IsDirectChannel,
 			default_channel.Members, default_channel.CreatedAt, default_channel.Messages},
 		Idempotent: true,
 	})
 
 	batch.Entries = append(batch.Entries, gocql.BatchEntry{
-		Stmt:       "UPDATE users SET rooms = rooms + {?} where id=? AND username=? AND email=?",
+		Stmt: `UPDATE users 
+           SET rooms = rooms + {?} 
+           where id=? AND username=? AND email=?`,
 		Args:       []interface{}{room.ID, room.Admin, admin_username, admin_email},
 		Idempotent: true,
 	})
@@ -86,14 +104,24 @@ func (r *Repository) JoinRoom(ctx context.Context, room_id gocql.UUID, user_id g
 
 	// check if user doesn't exist
 	var uid gocql.UUID
-	if err := r.db.Query(`SELECT ID FROM users WHERE id=?`, user_id).WithContext(ctx).Scan(&uid); err != nil {
+	if err := r.db.Query(`
+            SELECT 
+            id 
+            FROM users 
+            WHERE id=?`,
+		user_id).WithContext(ctx).Scan(&uid); err != nil {
 		log.Println("Error occured while executing query to get if user exists or not in join room repository function:", err)
 		return nil, errors.New("User id doesn't exist.")
 	}
 
 	// check if room doesn't exist
 	var rid gocql.UUID
-	if err := r.db.Query(`SELECT ID FROM rooms WHERE id=?`, room_id).WithContext(ctx).Scan(&rid); err != nil {
+	if err := r.db.Query(`
+            SELECT 
+            id 
+            FROM rooms 
+            WHERE id=?`,
+		room_id).WithContext(ctx).Scan(&rid); err != nil {
 		log.Println("Error occured while executing query to get if room exists or not in join room repository function:", err)
 		return nil, errors.New("Room id doesn't exist.")
 	}
